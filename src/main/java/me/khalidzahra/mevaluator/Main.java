c413cc2721ef0e8d53ecae55986821247d6fdd8f
khalidzahra <khalidzahra923@gmail.com>
2024-02-08 00:29:47 +0200

    Fix SizeAnalyzer not excluding spaces and comments

--- src/main/java/me/khalidzahra/mevaluator/Main.java
+++ src/main/java/me/khalidzahra/mevaluator/Main.java
@@ -2,17 +2,23 @@ package me.khalidzahra.mevaluator;
 
 import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.StaticJavaParser;
+import com.google.gson.Gson;
+import me.khalidzahra.mevaluator.analysis.MethodMetrics;
 import me.khalidzahra.mevaluator.analysis.analyzer.McCabeAnalyzer;
 import me.khalidzahra.mevaluator.analysis.analyzer.ReadabilityAnalyzer;
 import me.khalidzahra.mevaluator.analysis.analyzer.RevisionAnalyzer;
 import me.khalidzahra.mevaluator.analysis.analyzer.SizeAnalyzer;
 import me.khalidzahra.mevaluator.parse.JsonParser;
+import me.khalidzahra.mevaluator.parse.codeshovel.CSMethod;
 
-/**
- * Hello world!
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         ParserConfiguration config = new ParserConfiguration().setAttributeComments(false).setLanguageLevel(ParserConfiguration.LanguageLevel.RAW);
         StaticJavaParser.setConfiguration(config);
 
--- src/main/java/me/khalidzahra/mevaluator/analysis/analyzer/SizeAnalyzer.java
+++ src/main/java/me/khalidzahra/mevaluator/analysis/analyzer/SizeAnalyzer.java
@@ -3,12 +3,34 @@ package me.khalidzahra.mevaluator.analysis.analyzer;
 import me.khalidzahra.mevaluator.analysis.MethodMetrics;
 import me.khalidzahra.mevaluator.parse.codeshovel.CSMethod;
 
+import java.util.Scanner;
+
 public class SizeAnalyzer implements Analyzer {
 
     @Override
     public void analyze(CSMethod csMethod, MethodMetrics methodMetrics) {
-        int size = csMethod.getMethodDeclaration().getRange().map(methodRange -> methodRange.end.line - methodRange.begin.line + 1).orElse(0);
-        methodMetrics.setSize(size);
+        methodMetrics.setSize(getRealSize(csMethod));
+    }
+
+    private int getRealSize(CSMethod csMethod) {
+        int size = 0;
+        Scanner scanner = new Scanner(csMethod.getMethodSource());
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine().strip(); // Remove leading and trailing spaces
+            if (line.isEmpty() || line.startsWith("//")) continue;
+            if (line.startsWith("/*")) { // Check for multi-line comments
+                /*
+                    Only stop skipping when multi-line comment terminator is met.
+                    This also handles the case where a multi-line comment is on one line.
+                */
+                while (!line.endsWith("*/")) {
+                    line = scanner.nextLine().strip();
+                }
+                continue;
+            }
+            size++;
+        }
+        return size;
     }
 
 }
