1428a69a9ddfb526c1cce3c99103a7dbc4533957
khalidzahra <khalidzahra923@gmail.com>
2024-02-08 00:46:31 +0200

    Document ReadabilityUtil

--- src/main/java/me/khalidzahra/mevaluator/util/ReadabilityUtil.java
+++ src/main/java/me/khalidzahra/mevaluator/util/ReadabilityUtil.java
@@ -8,6 +8,11 @@ public class ReadabilityUtil {
 
     private static final PortableEvaluator EVALUATOR = new PortableEvaluator();
 
+    /**
+     * Uses the TSE readability JAR to calculate the readability metric of the original method sourcecode.
+     * @param csMethod CSMethod object loaded by Gson from the CodeShovel JSON files.
+     * @return Double containing the readability metric calculated for the method.
+     */
     public static double evaluate(CSMethod csMethod) {
         Function function = new Function(csMethod.getInitialMethodSource());
         return EVALUATOR.getReadability(function);
