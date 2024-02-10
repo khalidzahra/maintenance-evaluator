518beb6e731fcba524673b0783dd68e2aef75342
khalidzahra <khalidzahra923@gmail.com>
2024-02-10 05:41:13 +0200

    Prettify correlation evaluator output

--- correlation-evaluator/main.py
+++ correlation-evaluator/main.py
@@ -1,39 +1,51 @@
 import scipy
 import pandas as pd
+from tabulate import tabulate
 import sys
 
+
+# CSV file column headers
 cols = ["Size", "McCabe", "Readability"]
+
+
+# Prints a readable table from the values given to it.
+# Values should be a list containing each row as a list.
+def printTable(values):
+    table = [["Metrics", "Pearson", "p-value", "Spearman", "p-value", "Kendall's Tau", "p-value"]]
+    table.extend(values)
+    print(tabulate(table, headers="firstrow", tablefmt="simple_grid"))
+
+
+# Returns all input directories specified by user in command line arguments
+def parseInputFiles(args):
+    return args[1:]
+
 
 def main():
 
+    # Check if user specified CSV paths
     if len(sys.argv) < 2:
-        print("ERROR: Please include the CSV file path")
+        print("ERROR: Please include the CSV file paths")
         return
 
-    file_path = sys.argv[1]
+    for file_path in parseInputFiles(sys.argv):
+        try:
+            # Read CSV files as pandas dataframes
+            df = pd.read_csv(file_path, header="infer", skipinitialspace=True)
+            print()
+            print("======================= " + file_path + " =======================", "\n")
+            values = []
 
-    try:
-        df = pd.read_csv(file_path, header="infer", skipinitialspace=True)
-        print()
-        print("========= Pearson Correlation Coefficient =========\n")
-        for col in cols:
-            coeff = scipy.stats.pearsonr(df["#Revisions"], df[col])
-            print("#Revisions-" + col + ": ", coeff[0], " P-value: ", coeff[1])
-
-        print()
-        print("========= Spearman Correlation Coefficient =========\n")
-        for col in cols:
-            coeff = scipy.stats.spearmanr(df["#Revisions"], df[col])
-            print("#Revisions-" + col + ": ", coeff[0], " P-value: ", coeff[1])
-
-        print()
-        print("========= Kendall’s Tau Correlation Coefficient =========\n")
-        for col in cols:
-            coeff = scipy.stats.kendalltau(df["#Revisions"], df[col])
-            print("#Revisions-" + col + ": ", coeff[0], " P-value: ", coeff[1])
-    except:
-        print("ERROR: Invalid file path.")
-
+            # Compute correlations between Number of Revisions and other columns
+            for col in cols:
+                pearson = scipy.stats.pearsonr(df["#Revisions"], df[col])
+                spearman = scipy.stats.spearmanr(df["#Revisions"], df[col])
+                kendall = scipy.stats.kendalltau(df["#Revisions"], df[col])
+                values.extend([["#Revisions-" + col, pearson.statistic, pearson.pvalue, spearman.statistic, spearman.pvalue, kendall.statistic, kendall.pvalue]])
+            
+            printTable(values)
+        except:
+            print("ERROR: Invalid file path.")
 
 
 if __name__ == "__main__":
